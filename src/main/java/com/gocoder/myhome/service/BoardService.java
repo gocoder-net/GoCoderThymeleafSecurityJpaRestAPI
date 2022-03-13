407aa8b96adad3b6503caeb648d72c6d62c3785f
GoCoder Developer <gocoder.net@gmail.com>
2022-03-14 01:01:22 +0900

    init
    init

---
+++ src/main/java/com/gocoder/myhome/service/BoardService.java
@@ -0,0 +1,25 @@
+package com.gocoder.myhome.service;
+
+import com.gocoder.myhome.repository.BoardRepository;
+import com.gocoder.myhome.repository.UserRepository;
+import com.gocoder.myhome.model.Board;
+import com.gocoder.myhome.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Board save(String username, Board board) {
+        User user = userRepository.findByUsername(username);
+        board.setUser(user);
+        return boardRepository.save(board);
+    }
+
+}
